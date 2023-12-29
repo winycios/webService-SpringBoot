a2480ab507bb5caaafffb7d2ea2ebc4e9d1b1af4
Winycios <winycios.nascimento@sptech.school>
2023-12-29 02:26:56 -0300

    Exceptions CRUD

--- src/main/java/com/winy_group/demo/services/UserServices.java
+++ src/main/java/com/winy_group/demo/services/UserServices.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.winy_group.demo.entities.User;
 import com.winy_group.demo.repositories.UserRepository;
+import com.winy_group.demo.services.exceptions.DatabaseException;
 import com.winy_group.demo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserServices {
@@ -30,13 +35,24 @@ public class UserServices {
     }
 
     public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+        }catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User updatUser(Long id, User obj) {
+        try {
         User entity = userRepository.getReferenceById(id);
         updateData(entity, obj);
-        return userRepository.save(entity);
+        return userRepository.save(entity);    
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        
     }
 
     private void updateData(User entity, User obj) {
